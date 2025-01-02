1d4d92991d96b9f358cd2927ea85bbe55d77d4dc
Your Name <petrzhal@gmail.com>
2025-01-02 04:41:36 +0300

    update

--- Library/Library.Domain/obj/Debug/net8.0/Library.Domain.AssemblyInfo.cs
+++ Library/Library.Domain/obj/Debug/net8.0/Library.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206ea939a6aa629ba66f1bbe489e562a05bfe504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf694f681263bfcd0481626bfa48de5db8eae9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
